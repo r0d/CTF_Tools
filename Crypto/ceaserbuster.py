47e3e81463b1d6d985d57c1428ab26589cc25c10
Nick Rodriguez <nicolas.rodriguez@usma.edu>
2017-09-12 11:44:13 -0400

    Add files via upload
    Simple ceaser cipher solver via either brute force or known plaintext.

---
+++ Crypto/ceaserbuster.py
@@ -0,0 +1,35 @@
+string = input('Give meh a string: \n > ')
+inplaintext = input('If you know any plaintext give it here (else type "no"):  \n')
+
+string = string.lower()
+inplaintext = inplaintext.lower()
+#print(string)
+#print(inplaintext)
+
+for i in range(1, 27): 
+	shift = i
+	plaintext = '' 
+	for char in string: 
+		if ord(char) < 97 or ord(char) > 122: 
+			plaintext += char
+		elif ord(char) + i <= 122: 
+			plaintext += chr(ord(char)+i)
+		else: 
+			plaintext += chr(96 +(ord(char)+i - 122))  
+	#print(plaintext)
+	if inplaintext != 'no': 
+		if inplaintext in plaintext: 
+			print(plaintext) 
+			break
+		else: 
+			continue
+	else: 	
+		print(plaintext)
+		response = input("Does this look right? (Y/N) \n")
+		response = response.lower()
+		if response == 'y': 
+			print(plaintext)
+			break
+		else: 
+			continue
+#print("Couldn't find any") 
